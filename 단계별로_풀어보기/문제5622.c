02c9fd06eb1850e728a6d27657511d413f1ee762
k-ddk <rpekgus@gmail.com>
2020-07-27 14:37:54 +0900

    Add Cpp File [문제2941.c]

--- 단계별로_풀어보기/문제5622.c
+++ 단계별로_풀어보기/문제5622.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
 int main()
 {
 	char str[15];
 	int sec = 0, tmp;
 
 	gets(str);
-
 	for (int i = 0; i < strlen(str); i++)
 	{
 		if (str[i] >= 'W')
@@ -15,7 +13,7 @@ int main()
 			sec += 10;
 			continue;
 		}
-		else if (str[i] == 'S' || str[i] == 'V')
+		else if (str[i] == 'S' || str[i] == 'V')  //PQRS , 네가지씩 묶이게 되므로 알파벳이 세 개씩 묶이던 규칙이 깨져버림
 			tmp = str[i] - 66;
 		else
 			tmp = str[i] - 65;
@@ -25,6 +23,5 @@ int main()
 	}
 
 	printf("%d", sec);
-
 	return 0;
 }
