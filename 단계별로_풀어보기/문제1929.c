f84ed1dde8420a401066c3d118ac8f4b9a3b1507
k-ddk <rpekgus@gmail.com>
2020-08-10 15:17:59 +0900

    Update Cpp File [문제1929.c]

--- 단계별로_풀어보기/문제1929.c
+++ 단계별로_풀어보기/문제1929.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SIZE 1000000
 int main()
 {
-	int num[SIZE] = { 0, };  //왜 바로 num[1000000]은 안 되는 건지
+	int M, N, *arr, tmp;
 
-	printf("%d %d %d %d\n", num[0], num[1], num[2], num[3]);
+	scanf("%d%d", &M, &N);
 
+	arr = (int*)malloc(sizeof(int) * 1000001);
+	for (int i = 1; i < 1000001; i++)
+		arr[i] = 1;  //일단 다 소수라고 표시(=1)
 
-	return 0;
+	arr[1] = 0;
+	for (int i = 2; i <= N; i++)
+	{
+		if (arr[i] == 1)
+		{
+			for (int j = 2; j <= N/i; j++)
+					arr[i*j] = 0;
+		}
+	}
+
+	for (int i = M; i <= N; i++)
+	{
+		if (arr[i] == 1)
+			printf("%d\n", i);
+	}
+
+		return 0;
 }
