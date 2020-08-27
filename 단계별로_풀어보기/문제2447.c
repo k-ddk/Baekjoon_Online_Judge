5bb0591cf0a4fd020a9fd06ffbd82b824c135f14
k-ddk <rpekgus@gmail.com>
2020-08-27 16:13:51 +0900

    Add Cpp File...

--- 단계별로_풀어보기/문제2447.c
+++ 단계별로_풀어보기/문제2447.c
@@ -1,33 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void prin(int n);
+char **res;
+void prin(int n, int n1)
+{
+	if (n >= 3)
+	{
+		prin(n / 3, n1);  //재귀
+		for (int i = n / 3; i < n1; i += n)  //n만큼의 간격씩 
+		{
+			for (int j = n / 3; j < n1; j += n)  //n만큼의 간격씩
+			{
+				for (int k = i; k < i + n / 3; k++)  //(n/3)*(n/3) 크기의 공백 찍어내기
+				{
+					for (int l = j; l < j + n / 3; l++)
+						res[k][l] = ' ';
+				}
+			}
+		}
+	}
+
+}
 int main()
 {
 	int n;
 
 	scanf("%d", &n);
+	res = (char**)malloc(sizeof(char*)*n);
+	for (int i = 0; i < n; i++)
+		res[i] = (char*)malloc(sizeof(char)*n);  //이차배열 동적할당
 
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			res[i][j] = '*';  //우선 별로 채워두기
+	}
 
+	prin(n, n);
 
-
-
-
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			printf("%c", res[i][j]);
+		printf("\n");
+	}
 
 	return 0;
-}
-void prin(int n)
-{
-	for (int k = 0; k < n; k++)
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				if (i == 1 && j == 1)
-					printf(" ");
-				printf("*");
-			}
-			printf("\n");
-		}
-	}
 }
