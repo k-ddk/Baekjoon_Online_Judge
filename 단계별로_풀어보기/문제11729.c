227dc980b6123b161347225245a2b75a266eab48
k-ddk <rpekgus@gmail.com>
2021-01-01 16:01:44 +0900

    문제11729

--- 단계별로_풀어보기/문제11729.c
+++ 단계별로_풀어보기/문제11729.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <math.h>
+;
+void hanoi(int n, int from, int tmp, int to) {
 
-void hanoi()
+	if (n == 1) {  //마지막에 도달한 경우 재귀함수 탈출
+		printf("%d %d\n", from, to);
+	}
+	else {
+		hanoi(n - 1, from, to, tmp);
+		printf("%d %d\n", from, to);
+		hanoi(n - 1, tmp, from, to);
+	}
+
+};
 int main()
 {
-	int n, k;
+	int n, cnt;
 
 	scanf("%d", &n);
+	cnt = pow(2, n) - 1;
+	printf("%d\n", cnt);
 
-	k = pow(2, k) - 1;
-	printf("%d\n", k);
-
-
+	hanoi(n, 1, 2, 3);
 
 	return 0;
 }
